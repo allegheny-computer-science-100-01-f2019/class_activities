72e09bc73accc654bdd92025458ebca6747a64a1
jjumadin <jjumadin@aldenv64-6.local>
2019-09-08 14:31:52 -0400

    Repo set up and a program for Sep 9 class

---
+++ src/instructor/PrintExample.java
@@ -0,0 +1,20 @@
+/** PrintExample program in Java.
+* This program shows examples of String
+* concatenation and escape sequence usage.
+* @author Janyl Jumadinova
+*/
+
+public class PrintExample {
+
+  /** main method containing print statements
+  * to demonstrate String printing and String
+  * concatenation.
+  */
+  public static void main (String [] args) {
+    System.out.println("Hello World!");
+    System.out.print("Hi\t");
+    System.out.print("there! \n");
+    System.out.println("Adding " + 12 + " "+ 23);
+    System.out.println("Adding "+ (12 + 23));
+  }
+}
